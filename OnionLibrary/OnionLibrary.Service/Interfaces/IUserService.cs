1bbb6f5040cf2b3271d6808fab10d2dd1062dad5
Neek <Sp-Neek@yandex.ru>
2022-08-16 17:22:16 +0700

    Реализация сервиса(Прослойки между контроллерами и бд)

---
+++ OnionLibrary/OnionLibrary.Service/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using OnionLibrary.Domain.Models;
+using OnionLibrary.Domain.Response;
+using OnionLibrary.Domain.ViewModels.Book;
+
+namespace OnionLibrary.Service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IBaseResponse<IEnumerable<User>>> GetBooks();
+        Task<IBaseResponse<User>> GetBook(int id);
+        Task<IBaseResponse<bool>> DeleteBook(int id);
+        Task<IBaseResponse<UserViewModel>> CreateBook(UserViewModel bookViewModal);
+        Task<IBaseResponse<User>> EditBook(int id, UserViewModel model);
+    }
+}
